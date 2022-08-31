6e2538f41e7a05e0cb51cfd02dd743291edad320
Kassie Bradshaw <kassie.r.bradshaw@gmail.com>
2022-08-31 16:31:30 -0700

    Update app.js

--- class-06/in-class-demo/app.js
+++ class-06/in-class-demo/app.js
@@ -1,32 +1,53 @@
 'use strict';
 
-// declare a variable
-let myName;
-console.log(myName);
-// reassign the variable
-myName = 'Kassie';
-console.log(myName);
-// reassign it again
-myName = 'Allen';
-console.log(myName);
+// get proof of life, check the browser's developer Console
+// console.log('Hello World!');
 
-console.log('Hello World!');
-
+-----------------------------------------------------
+  
 // declare a variable called x, assign it value of the value 10
 let x = 10;
 console.log(x);
 
+-----------------------------------------------------
+
+// declare a variable called myName, it has no value
+let myName;
+
+// print the value of the variable to the console
+console.log(myName);
+
+// reassign the variable and print the new value
+myName = 'Kassie';
+console.log(myName);
+
+// reassign it again and print the new value
+myName = 'Allen';
+console.log(myName);
+
+-----------------------------------------------------
+
+// prompt the user for some input, so you can use it later.
+// hold its value in a variable called usersName
 let usersName = prompt('What is your name?');
+
+// declare a variable called message, it has no value (yet)
 let message;
 
-//structure of if/else
-// if(condition to evaluate){code to execute}else{code to execute}
+//structure of if/else conditional statement
+// if (this condition evaluates to true) {execute this code} else {execute this code}
 
 // if (usersName == 'Kassie'){
 //   message = 'Hiya Teach!'
 // } else {
 //   message = 'Welcome to my site!';
 // }
+
+-----------------------------------------------------
+// how we can check multiple conditions using if/else if/else
+  
+// structure for this is:
+// if (this condition evaluates to true) {execute this code} else if (this condition evaluates to true) {execute this code} else {execute this code} 
 
 if (usersName == 'Kassie'){
   message = 'Hiya Teach!'
@@ -37,8 +58,9 @@ if (usersName == 'Kassie'){
 } else {
   message = 'Welcome to my site!';
 }
+
+// how we can use Javascript to put stuff on our HTML page
+// document.write means write what's in the () to the HTML document
 document.write('Hi ' + usersName);
 document.write(message);
-// console.log(usersName);
-// message = 'Hi, ' + usersName + '!';
-// console.log(message);
+
